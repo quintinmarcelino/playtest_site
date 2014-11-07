970dd9191961e78b4d9d31a51d29f779573ecc07
Quintin <quintinmarcelino@gmail.com>
2014-11-06 21:43:16 -0500

    Commit 2: Users and Games working, routes up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :games
-
+root 'games#index'
+get '/users' => 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'games#index'
+  
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
