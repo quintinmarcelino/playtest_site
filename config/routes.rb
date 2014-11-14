023de8acaa1704f7bc4093f009db4d3d0556e761
Quintin <quintinmarcelino@gmail.com>
2014-11-13 21:50:39 -0500

    Fixed the games view slightly

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :games
 root 'games#index'
 get '/users' => 'users#index'
+#destroy '/users/sign_out' => ''
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
