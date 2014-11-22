c9ae6ad65e386afbbeb771bb703b485692791ef6
Quintin <quintinmarcelino@gmail.com>
2014-11-21 21:49:52 -0500

    Fixed Date stuff, made a welcome page and made username's unique.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
-            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :birthday) }
+            devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :current_password, :birthday, :favorite_game, :name) }
         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
   resources :users
   resources :games
-root 'games#index'
+root "welcome#index"
+get "games" => "games#index"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
